52c9b55d65d3e9d80e88a932974f8a4b9e30a5fc
voland1711 <92478610+voland1711@users.noreply.github.com>
2023-03-02 13:47:33 +0500

    Тестовая

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,9 +1,13 @@
 package ru.practicum.shareit;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@AutoConfigureTestDatabase
 @SpringBootTest
 class ShareItTests {
+
+	@Test
+	void contextLoads() {
+	}
 
 }
