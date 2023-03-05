733d14c4199c195ab08268a6febf50b3ac5ef552
ViktorBuzmakov <buzmak-1711@rambler.ru>
2023-03-06 00:48:01 +0500

    Внес изменения, в соотвествии с полученными замечаниями + устранил ошибку Error:  src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java:[20,8] (imports) UnusedImports: Unused import - java.util.function.Consumer

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -17,7 +17,6 @@ import ru.practicum.shareit.user.repository.UserRepository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 @Slf4j
