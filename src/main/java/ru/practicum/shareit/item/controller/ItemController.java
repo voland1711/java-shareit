c8bad01672aa6d2e2466ecca5ee4964f07f5aa3d
voland1711 <92478610+voland1711@users.noreply.github.com>
2023-04-09 16:16:41 +0500

    Update ItemController.java
    Тест № 1

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.item.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.booking.exception.BadRequestException;
 import ru.practicum.shareit.comment.dto.CommentDto;
 import ru.practicum.shareit.comment.dto.CommentShortDto;
 import ru.practicum.shareit.comment.service.CommentService;
@@ -23,40 +24,41 @@ import java.util.List;
 public class ItemController {
     private final ItemService itemService;
     private final CommentService commentService;
+    private static final String SHARER_USER_ID = "X-Sharer-User-Id";
 
     @GetMapping
-    public List<ItemDto> getAllItems(@RequestHeader("X-Sharer-User-Id") long userId) {
+    public List<ItemDto> getAllItems(@RequestHeader(SHARER_USER_ID) long userId) {
         log.info("Работает: ItemController.getAllItems");
         return itemService.getAllItems(userId);
     }
 
     @GetMapping("/{itemId}")
-    public ItemDto getItemById(@RequestHeader("X-Sharer-User-Id") long userId, @PathVariable long itemId) {
+    public ItemDto getItemById(@RequestHeader(SHARER_USER_ID) long userId, @PathVariable long itemId) {
         log.info("Работает: ItemController.getItemById");
         return itemService.getItemById(userId, itemId);
     }
 
     @PostMapping
-    public ItemDto createItem(@RequestHeader("X-Sharer-User-Id") long userId, @Valid @RequestBody ItemDto itemDto) {
+    public ItemDto createItem(@RequestHeader(SHARER_USER_ID) long userId, @Valid @RequestBody ItemDto itemDto) {
         log.info("Работает: ItemController.createItem");
-        return itemService.createItem(userId, itemDto);
+        throw new BadRequestException("Вещь уже забронирована");
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDto updateItem(@RequestHeader("X-Sharer-User-Id") long userId, @RequestBody Item item,
+    public ItemDto updateItem(@RequestHeader(SHARER_USER_ID) long userId, @RequestBody Item item,
                               @PathVariable long itemId) {
         log.info("Работает: ItemController.updateItem");
         return itemService.updateItem(userId, item, itemId);
     }
 
     @GetMapping("/search")
-    public List<ItemDto> searchItems(@RequestHeader("X-Sharer-User-Id") long userId, @RequestParam("text") String text) {
+    public List<ItemDto> searchItems(@RequestHeader(SHARER_USER_ID) long userId, @RequestParam("text") String text) {
         log.info("Работает: ItemController.searchItems");
         return itemService.searchItems(userId, text);
     }
 
     @PostMapping("/{itemId}/comment")
-    public CommentDto createComment(@PathVariable Long itemId, @RequestHeader("X-Sharer-User-Id") Long userId,
+    public CommentDto createComment(@PathVariable Long itemId, @RequestHeader(SHARER_USER_ID) Long userId,
                                     @Valid @RequestBody CommentShortDto commentShortDto) {
         log.info("Работает: ItemController.createComment");
         return commentService.createComment(itemId, userId, commentShortDto);
