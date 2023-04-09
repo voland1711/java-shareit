d16840c3b9ecb9960dff4d1be62c35eeeaf412c8
voland1711 <92478610+voland1711@users.noreply.github.com>
2023-04-09 19:37:16 +0500

    Update ItemRepository.java

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -1,8 +1,10 @@
 package ru.practicum.shareit.item.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Item;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
 }
